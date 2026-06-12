7d2ab6f778e34589939d51c4e688bae187b7be7c
markleenders <68488882+markleenders@users.noreply.github.com>
2026-06-12 12:19:34 +0200

    Add version and author to _meta.lua

--- _meta.lua
+++ _meta.lua
@@ -1,6 +1,9 @@
+-- SPDX-License-Identifier: AGPL-3.0-or-later
 local _ = require("gettext")
 return {
     name = "stopwatchtimer",
     fullname = _("StopwatchTimer"),
+    version = "1.0.4",
+    author = "Mark Leenders",
     description = _([[Shows a stopwatch or a timer in a fullscreen display.]]),
 }
